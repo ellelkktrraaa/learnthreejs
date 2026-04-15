9a66946510175640f9cdd02cbef15b474ddd10f8
ian <1499665314@qq.com>
2026-04-16 01:40:09 +0800

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './',
   server: {
     port: 5173,
     open: true
@@ -9,6 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html'
